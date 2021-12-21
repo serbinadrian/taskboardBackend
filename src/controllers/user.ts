ccf8a4aa74fbeed56de4392261e53c8aa5edc562
Nazar Mineev <mineevnazar@gmail.com>
2021-12-21 17:50:00 +0300

    Minor fix getUser

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -5,9 +5,9 @@ import {User} from "../interfaces/User";
 export const signInUser = (request: Request, response: Response) => {
     const userCredits = request.body;
 
-    getUser(userCredits, (error: Error, user: User) => {
-        if (user) {
-            response.send(user)
+    getUser(userCredits, (error: Error, user: Array<User>) => {
+        if (user.length) {
+            response.send(user.pop());
         } else {
             response.status(404);
             response.end("Not found");
